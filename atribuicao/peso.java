bab7c7fc59ff21005de8b2b2bf0674fd4d60f33b
Alex Alves da Silva <167913877+alexalves011@users.noreply.github.com>
2025-08-25 21:32:59 -0300

    Add files via upload

---
+++ atribuicao/peso.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class peso {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Digite seu peso");
+        float peso1 = leia.nextFloat();
+        System.out.println("Seu peso é" + peso1);
+    }
+}
