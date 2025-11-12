53ed163bd59484c9aeb6cbf5c369d2f091cd6b8e
alexalves011 <alexalves.work@gmail.com>
2025-11-12 18:39:26 -0300

    Corrigindo bug Aula PW

--- Programação Web/05-11/scripit.js
+++ Programação Web/05-11/scripit.js
@@ -1,25 +1,24 @@
-const inputNome = document.getElementById('camponome');
-const inputSobrenome = document.getElementById('campoSobreNome');
-const selectTurno = document.getElementById('campoturno');
+const inputNome = document.getElementById("camponome");
+const inputSobrenome = document.getElementById("campoSobreNome");
+// Agora esta linha vai funcionar, pois "campoturno" existe no HTML
+const selectTurno = document.getElementById("campoturno");
 
-const resultado = document.getElementById('resultado');
+const resultado = document.getElementById("resultado"); // A variável se chama 'resultado'
 
-function botaoMostrar(){
+function botaoMostrar() {
+  const nome = inputNome.value;
+  const turno = selectTurno.value;
 
-    const nome = inputNome.value;
-
-    const turno = selectTurno.value;
-
-    if (nome.trim() === ""){
-        divresultado.innerHTML = "Por Favor digite um nome"
-
-    } else{
-        const mensagem = `
+  if (nome.trim() === "") {
+    // CORREÇÃO 2: Usei 'resultado' ao invés de 'divresultado'
+    resultado.innerHTML = "Por Favor digite um nome";
+  } else {
+    const mensagem = `
         Olá ${nome}  !!
         Você está matriculado no turno ${turno}
         `;
 
-        divresultado.innerHTML = mensagem
-    }
-
+    // CORREÇÃO 3: Usei 'resultado' ao invés de 'divresultado'
+    resultado.innerHTML = mensagem;
+  }
 }
